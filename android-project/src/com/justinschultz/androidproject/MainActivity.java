52092022a0bc680370db2ac58449ca405074d332
Jared <jaredandrews@wpi.edu>
2012-06-27 14:29:04 -0400

    Modified build file.

--- android-project/src/com/justinschultz/androidproject/MainActivity.java
+++ android-project/src/com/justinschultz/androidproject/MainActivity.java
@@ -1,13 +1,8 @@
 package com.justinschultz.androidproject;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
